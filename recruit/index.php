a3ff53f8e557976ca77a7ac1eeb4db8c66bbf336
Aye Thin Myat <156531412+EriThinMyat@users.noreply.github.com>
2024-03-11 11:44:53 +0630

    Add files via upload

---
+++ recruit/index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<title>採用に関する質問｜東京のWeb制作会社 クーシー（COOSY）</title>
+<meta name="description" content="クーシーの採用に関するよくある質問・FAQを掲載しています。採用面接などでいただく質問をQ&A形式でまとめました。一緒にクーシーをつくっていってくれる人との出会いを待っています。" />
+<?php include("../../include/head_favicon.php"); ?>
+<?php include("../../include/head_css.php"); ?>
+<!-- ページ個別CSS -->
+<link rel="stylesheet" href="/assets/css/recruit/index.css">
+<link rel="stylesheet" href="/assets/css/common.css">
+
+<!-- /ページ個別CSS -->
+<?php include("../../include/head_js.php"); ?>
+</head>
+<body>
+  <?php include("../../include/header.php"); ?>
+  <main class="main">
+      <div class="main__inner__section">
+        <div class="main__keyVisual">
+          <img class="main__keyVisual__image" src="/assets/img/recruit/recruit_kv.png" alt="リクルート">
+          <section class="main__keyVisual__lead">
+            <h2 class="main__keyVisual__title">
+              クーシーが考える「新しい働き方」とは？
+            </h2>
+            <p class="main__keyVisual__text section__text">1999年に設立したクーシーはインターネットによる変革で、既存のルールに捕らわれない新しいビジネスが生まれ、そこで活躍できると確信していました。それから20年近くの時が過ぎインターネットは成熟期を迎えています。そして今、働き方に新しい変革が生まれています。新しい働き方とはどのように写っているのでしょうか？クーシーが考える新しい働き方を大きくまとめると３つになります。</p>
+          </section>
+        </div>
+        <section class="main__section">
+          <div class="main__section__item">
+            <div class="main__section__Contents">
+              <h3 class="section__title">１. フラットな関係は「信頼関係」から</h3>
+              <p class="section__text">会社が働く人の様々な事情や多様な価値観を受け入れられないこともあります。個を大切にするあまり、会社の効率を損なう場合もあります。それぞれの事情がある以上、それぞれの価値観は異なります。お互い受け入れることが大切。ベースとしてマジメに仕事に取り組む姿勢が「信頼関係」を築きます。そして「信頼関係」をベースにお互いの成果に向き合い効率化していくことでフラットな関係を築けるのではないかと思います。</p>
+            </div>
+            <div class="main__section__Contents">
+              <h3 class="section__title">2. 役職ではなく、成果と貢献</h3>
+              <p class="section__text">「信頼関係」をベースとしたフラットな関係では、役職はただの役割でしかないと思います。この役割の中でみんなの成果に貢献した人を正当に評価します。成果というと利益や成長などが浮かびますが、そのプロセスには様々な要因があります。会社ではなく、みんなが成果をあげることを考えて行動する。そうすることで組織は効率化していくと思います。</p>
+            </div>
+            <div class="main__section__Contents">
+              <h3 class="section__title">３. 働きやすい環境はみんなで創る</h3>
+              <p class="section__text">働きやすさは人それぞれによって異なります。働く人が個々の働きやすさで行動すると、他の人の働きやすさを阻害するケースが多くあります。まずは「信頼関係」を築き、今の成果と貢献を評価して、お互いを尊重することによって会社の利益は上がります。上がった利益は会社で働く人全員の恩恵に繋がります。だから働きやすい環境をみんなで創ることが必要です。</p>
+            </div>
+            <p class="section__text">今、世の中では「働き方改革」が提唱されています。労働時間を減らしたり、有給休暇は義務化されています。クーシーでも当然この基準は守られていますが、クーシーが考える「働き方改革」はその先を見ています。色々な事情を持った人たちが働きやすい環境を創る。クーシーが考える「働き方改革」です。例えば介護のため会社に行けない人、田舎で生まれ育ち仕事に恵まれない人。そもそも働く時間や場所は必要な条件ではありません。それぞれの事情に合わせて好きな時間、好きな場所で働けば良いと思っています。
+              その前提条件が「信頼関係」であり「成果と貢献」での評価であり「みんなで創る」ことだと思います。お互いの事情を理解しながら、お互いの都合で働き方を決め、良いカンケーを気づいていく。</p>
+            <p class="section__text mt-30 mb-10">それが「都合の良いカンケー」です。</p>
+            <h3 class="section__title">あなたも「都合の良いカンケー」を築きませんか？</h3>
+          </div>
+        </section>
+        <section class="main__section recuritTypes">
+          <ul class="main__section__recuritTypes">
+            <li class="item">
+              <section class="item__inner">
+                <img class="item__image" src="/assets/img/recruit/graduate_img01.png" alt="新卒採用">
+                <div class="item__link new__graduate">
+                  <p class="item__link__text">新卒採用(東京本社)<span><img src="/assets/img/recruit/shape.png" alt="New Graduate Link"></span></p>
+                </div>
+                <p class="item__text">東京本社勤務希望の新卒・第二新卒採用についてはこちらから。</p>
+              </section>
+            </li>
+            <li class="item">
+              <section class="item__inner">
+                <img class="item__image" src="/assets/img/recruit/career_img02.png" alt="Mid Career">
+                <div class="item__link mid__career">
+                  <p class="item__link__text">新卒採用(東京本社)<span><img src="/assets/img/recruit/shape.png" alt="中途採用"></span></p>
+                </div>
+                <p class="item__text">東京本社勤務希望の中途採用についてはこちらから。</p>
+              </section>
+            </li>
+            <li class="item">
+              <section class="item__inner">
+                <img class="item__image" src="/assets/img/recruit/laboratory_img03.png" alt="クーシーラボ岩手採用">
+                <div class="item__link coopsy__lab">
+                  <p class="item__link__text">新卒採用(東京本社)<span><img src="/assets/img/recruit/shape.png" alt="Coopsy Laboratory Link"></span></p>
+                </div>
+                <p class="item__text">クーシーラボ岩手勤務希望の新卒・中途採用についてはこちら。</p>
+              </section>
+            </li>
+          </ul>
+        </section>
+        <section class="main__section">
+          <div class="main__section__jobCategory">
+            <div class="section__jobCategory__title">
+              <p class="sub">他にもこんな働き方もあります。</p>
+              <h2 class="title">Browse <span class="br">by Job Category.</span></h2>
+            </div>
+            <div class="section__jobCategory__content">
+              <div class="jobCategory__partime">
+                <div class="jobCategory__partime__title">
+                  <div class="jobCategory__text">
+                    <p class="sub">実際に質問を受けた</p>
+                    <h4 class="title">アルバイト・パート</h4>
+                  </div>
+                </div>
+                <p class="section__text">仕事を通じてHTML、広告、ライティングなどWeb制作に関わる様々なスキルを身につけることができます。元Web業界のママさんワーカーも歓迎です。</p>
+                <p class="jobCategory__link">
+                  <a href="#">アルバイト・パートタイム</a>
+                </p>
+              </div>
+              <div class="jobCategory__internship">
+                <div class="jobCategory__internship__title">
+                  <div class="jobCategory__text">
+                    <p class="sub">職業体験したい！</p>
+                    <h4 class="title">インターンシップ</h4>
+                  </div>
+                </div>
+                <p class="section__text">就職前にインターネットの業務に携わりたい方、デザインやプログラムを職業体験したい方を東京・岩手ともに募集しています。</p>
+                <p class="jobCategory__link">
+                  <a href="#">インターンシップ</a>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="main__section__question">
+            <div class="section__question__title">
+              <p class="sub">採用やインターンなど働き方について質問が...</p>
+              <h2 class="title">Well, <span class="br">I have a question ...</span></h2>
+            </div>
+            <div class="section__question__content">
+              <div class="question__partime">
+                <div class="question__partime__title">
+                  <div class="question__text">
+                    <p class="sub">実際に質問を受けた</p>
+                    <h4 class="title">よくある質問集</h4>
+                  </div>
+                </div>
+                <p class="section__text">面接や会社見学でいらっしゃった方々が実際にご質問いただいた「よくある質問」をまとめてみました。お問い合わせの前にお読みください。</p>
+                <p class="question__link">
+                  <a href="#">よくある質問</a>
+                </p>
+              </div>
+              <div class="question__internship">
+                <div class="question__internship__title">
+                  <div class="question__text">
+                    <p class="sub">実際に質問を受けた</p>
+                    <h4 class="title">よくある質問集</h4>
+                  </div>
+                </div>
+                <p class="section__text">募集内容からインターンシップまでリクルートに関わる内容について直接質問したい方は、お問い合わせフォームからお問い合わせください。</p>
+                <p class="question__link">
+                  <a href="#">採用についてのお問い合わせ</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="main__gallery">
+          <div class="main__gallery__item">
+            <div class="gallery__item__inner">
+              <div class="main__gallery__image">
+                <img src="/assets/img/recruit/img01.png" alt="Recurit Image01">
+              </div>
+              <div class="main__gallery__image">
+                <img src="/assets/img/recruit/img02.png" alt="Recurit Image02">
+              </div>
+            </div>
+            <div class="gallery__item__inner">
+              <div class="main__gallery__image">
+                <img src="/assets/img/recruit/img05.png" alt="Recurit Image05">
+              </div>
+              <div class="main__gallery__image">
+                <img src="/assets/img/recruit/img06.png" alt="Recurit Image06">
+              </div>
+            </div>
+           
+          </div>
+          <div class="main__gallery__item">
+            <div class="gallery__item__inner">
+              <div class="main__gallery__image">
+                <img src="/assets/img/recruit/img03.png" alt="Recurit Image02">
+              </div>
+              <div class="main__gallery__image">
+                <img src="/assets/img/recruit/img04.png" alt="Recurit Image04">
+              </div>
+            </div>
+            <div class="gallery__item__inner">
+              <div class="main__gallery__image">
+                <img src="/assets/img/recruit/img07.png" alt="Recurit Image07">
+              </div>
+              <div class="main__gallery__image">
+                <img src="/assets/img/recruit/img08.png" alt="Recurit Image08">
+              </div>
+            </div>
+           
+          </div>
+        </section>
+      </div>
+    </main>
+  <?php
+    $footer_params = array(
+      array('name' => 'リクルート', 'url' => '/recruit/'),
+      array('name' => 'よくある質問', 'url' => ''),
+    );
+  ?>
+  <?php include("../../include/footer.php"); ?>
+</body>
+</html>
